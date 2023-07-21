2d9b9a2872b851fcf2a2b4f2d0a52ebefee4296f
mailorcanto <mailorcanto@gmail.com>
2023-07-21 12:28:31 -0300

    atualizando userController

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -2,68 +2,21 @@ const express = require('express');
 
 import userController from '../controller/userController';
 
-
-//Instancia o express na variável app
-//const app = express();
+//Instancia o express na variável router
 const router = express.Router();
-//app.use(express.json()); //comando para o express usar o json
 
 //listar usuários
-router.get('/users', userController.listUser);
+router.get('/users', userController.getUser);
 
 //Cadastrar usuario
 router.post('/users', userController.postUser);
 
-/*
 //Alterar usuário
-router.put('/user/:id', (req: any, res: any) => {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
-
-    //recupera o id enviado por parametro
-    const userId = req.params.id;
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //atribui os dados da requisição ao usuario existente na base de dados
-    content[userId] = req.body;
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-    
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
-
-    //retorno amigável para o usuário que o endpoint
-    res.send(`User with id ${userId} has been updated`)
-});
-
+router.put('/user/:id', userController.putUser);
 
 //Deletar usuário
-router.delete('/user/:id', (req: any, res: any) => {
-    //atribui a base de dados em nova variavel
-    const jsonDataBase = fs.readFileSync(data);
+router.delete('/user/:id', userController.deleteUser);
 
-    //recupera o id enviado por parametro
-    const userId = req.params.id; //atributo params
-    
-    //analisa string JSON e transforma em um objeto JavaScript
-    let content = JSON.parse(jsonDataBase);
-    
-    //delete
-    delete content[userId];
-    
-    //analisa um objeto em JavaScript e transforma em uma string JSON
-    const values = JSON.stringify(content);
-
-    //lê o arquivo da base de dados e adiciona o novo objeto
-    fs.writeFileSync(data, values);
-
-    //retorno amigável para o usuário que o endpoint
-    res.send(`User with id ${userId} has been deleted`);
-});
-*/
 
 export default router; //o que foi construído em app.ts está disponível para acesso em outros locais 
 
